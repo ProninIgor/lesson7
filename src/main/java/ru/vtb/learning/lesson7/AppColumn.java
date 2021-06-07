9f32ec9ce81d14462e4a77301926b6a113b96a79
Igor Pronin <madman-90@list.ru>
2021-06-07 16:56:40 +0400

    second

---
+++ src/main/java/ru/vtb/learning/lesson7/AppColumn.java
@@ -0,0 +1,12 @@
+package ru.vtb.learning.lesson7;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AppColumn {
+    String name();
+}
--- src/main/java/ru/vtb/learning/lesson7/MainApp.java
+++ src/main/java/ru/vtb/learning/lesson7/MainApp.java
@@ -4,4 +4,12 @@ public class MainApp {
     public static void main(String[] args) {
 
     }
+
+    public static String createScript(Class entityType){
+        if(entityType.isAnnotationPresent(AppTable.class)){
+            throw new IllegalArgumentException("Invalid class");
+        }
+
+        return "";
+    }
 }
